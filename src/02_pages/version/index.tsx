a7c06c7e4c5394fcb2429b8714c08b4ab6a8d42e
Stepan <stepan.sharifulin@mail.ru>
2024-01-15 16:14:36 +0500

    Update version

--- src/02_pages/version/index.tsx
+++ src/02_pages/version/index.tsx
@@ -4,8 +4,8 @@ type VersionPageProps = {}
 
 const VersionPage: React.FC<VersionPageProps> = ({}) => {
     return (
-        <div className="max-w-screen h-screen">
-            Version 0.2.1
+        <div className="max-w-screen h-screen p-6">
+            Version 0.1.2
         </div>
     );
 };
