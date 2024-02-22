577895514c6d050d0eb337014c636f40603acd2e
Stepan <stepan.sharifulin@mail.ru>
2024-02-22 15:36:13 +0500

    Fix
    - Fix nginx conf for ssl (added host for inner address backend)
    - Fix base url. There was problem with link '/api'. When we update page this link deleted

--- src/06_shared/api/baseApi.ts
+++ src/06_shared/api/baseApi.ts
@@ -1,36 +1,47 @@
 import {getEnv} from "../../05_entities/FetchEnv";
 
-let baseUrl: string = '';
+const getBaseUrl = (): Promise<string> => {
+    return new Promise((resolve) => {
+        getEnv()
+            .then((process) => {
+                const baseUrl = [
+                    process.env.REACT_APP_API_PROTOCOL, "://",
+                    process.env.REACT_APP_SERVER_PATH, ":",
+                    process.env.REACT_APP_SERVER_PORT, "/api"
+                ].join('');
+                resolve(baseUrl)
+            })
+    })
+}
 
 export const fetchData = <T>(url: string, init?: RequestInit): Promise<T> => {
     return fetch(url, init)
         .then(response => response.json()) as Promise<T>
 }
 
-(async () => {
-    let process = await getEnv()
-    baseUrl = `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_SERVER_PATH}:${process.env.REACT_APP_SERVER_PORT}/api`
-})()
-
-const fetchDataBlob = (url: string, init?: RequestInit): Promise<Blob> => {
-    return fetch(url, init).then(resp => resp.blob())
-}
-
 export const baseApi = <T>(url: string, init?: RequestInit): Promise<T> => {
-    return fetchData<T>(baseUrl + url, {
-        // credentials: 'include',
-        headers: {
-            'Content-Type': "application/json"
-        },
-        ...init,
+    return new Promise<T>((resolve) => {
+        getBaseUrl()
+            .then(baseUrl => {
+                resolve(fetchData<T>(baseUrl + url, {
+                    // credentials: 'include',
+                    headers: {
+                        'Content-Type': "application/json"
+                    },
+                    ...init,
+                }))
+            })
     })
 }
 
-export const baseApiMultipart = <T>(url: string, init?: RequestInit): Promise<T> =>
-    fetchData<T>(baseUrl+url, {
-        // credentials: 'include',
-        ...init
+export const baseApiMultipart = <T>(url: string, init?: RequestInit): Promise<T> => {
+    return new Promise<T>(resolve => {
+        getBaseUrl()
+            .then(baseUrl => {
+                resolve(fetchData<T>(baseUrl+url, {
+                    // credentials: 'include',
+                    ...init
+                }))
+            })
     })
-
-export const baseApiBlob = (url: string, init?: RequestInit): Promise<Blob> =>
-    fetchDataBlob(baseUrl+url, init)
+}
