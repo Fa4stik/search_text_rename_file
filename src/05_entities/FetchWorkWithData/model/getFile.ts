f1bb5c6586b25ba082535304917741b65a649d0a
Stepan <stepan.sharifulin@mail.ru>
2024-02-26 23:33:07 +0500

    Update
    - Add dockerfile dev and prod
    - Add dynamic sheme for backend picture response

--- src/05_entities/FetchWorkWithData/model/getFile.ts
+++ src/05_entities/FetchWorkWithData/model/getFile.ts
@@ -1,5 +1,21 @@
 import {baseApi} from "../../../06_shared/api/baseApi";
+import {getEnv} from "../../FetchEnv";
 
-export const getFile = (uid: number, pdf_id?: string): Promise<string> => pdf_id
-    ? baseApi('/get-file?' + new URLSearchParams({uid: uid.toString(), pdf_id}))
-    : baseApi('/get-file?' + new URLSearchParams({uid: uid.toString()}))
+export const getFile = (uid: number, pdf_id?: string): Promise<string> =>
+    new Promise((resolve) => {
+        let myPromise = baseApi<string>('/get-file?' + new URLSearchParams({uid: uid.toString()}))
+        if (pdf_id)
+            myPromise = baseApi<string>('/get-file?' + new URLSearchParams({uid: uid.toString(), pdf_id}))
+        myPromise
+            .then(shortPath => {
+                getEnv()
+                    .then(process => {
+                        resolve([
+                            window.location.protocol, '//',
+                            process.env.REACT_APP_SERVER_PATH, ':',
+                            process.env.REACT_APP_SERVER_PORT,
+                            shortPath
+                        ].join(''))
+                    })
+            })
+    })
